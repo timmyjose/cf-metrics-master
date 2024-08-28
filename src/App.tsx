58dd6bd5354d22b5052ac73bbd255712e82d2920
Timmy Jose <zoltan.jose@gmail.com>
2024-08-28 22:26:53 +0530

    wip: attempting to generate proof and verify it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
+import { Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
 import Home from './screens/Home'
 import Demo from './screens/Demo'
+import * as Linking from 'expo-linking'
+
+// Set up Deep Links support
+const prefix = Linking.createURL('/')
 
 export type RootStackParamsList = {
   Home: undefined
   Demo: undefined
+  PostReclaim: undefined
 }
 
 const Stack = createNativeStackNavigator<RootStackParamsList>()
 
 const App = () => {
+  const linking = {
+    prefixes: [prefix, 'cfmm://', 'cfmm:'],
+    config: {
+      screens: {
+        Demo: 'demo'
+      }
+    }
+  }
+
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
       <Stack.Navigator>
         <Stack.Screen name='Home' component={Home} />
         <Stack.Screen name='Demo' component={Demo} />
